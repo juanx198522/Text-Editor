c4d536f44a73b55297c99e1188be6e3de8664a34
juanx198522 <juanx1985@icloud.com>
2023-09-04 21:00:57 -0400

    Database setted

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
       database: './src/js/database.js',
       editor: './src/js/editor.js',
       header: './src/js/header.js',
